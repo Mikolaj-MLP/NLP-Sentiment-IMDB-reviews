87445180a1db3bdf39d5ec7b9ed1f689d5cdecff
Mikołaj Mielewczyk <mikolajmielewczyk@wp.pl>
2025-04-03 22:01:09 +0200

    Changes to the usage output

--- Trained_models_usage.py
+++ Trained_models_usage.py
@@ -2,7 +2,9 @@ from utils.predict_sentiment import PredictSentiment
 
 predictor = PredictSentiment(save_dir="Trained_models", vocab_path="Trained_models/vocab.pt")
 
-review = "I really like this film, it showed me many interesting things and therefore i will recommend it to others i case they have not yet seen it"
+review = '''
+I really liked this movie. Just kidding I lied.
+'''
 
 prediction = predictor.predict(review)
 
